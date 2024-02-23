fde20188332d28791ac8d0f98dc86812d07597db
Asiri Sandaruwan <1997asiri12@gmail.com>
2024-02-24 02:02:02 +0530

    Search function is done

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -26,26 +26,46 @@ const ProductList = () => {
         }
     }
 
+    const searchHandler = async (event) =>{
+        let key = event.target.value;
+        if(key){
+            let result = await fetch(`http://localhost:5000/search/${key}`);
+            result = await result.json();
+            if(result){
+                setProducts(result);
+            }
+        }
+        else{
+            getProducts();
+        }
+    }
+
     return (
         <div className="product-list">
             <h3>Product List</h3>
-            <ul>
-                <li>S. No.</li> 
-                <li>Name</li> 
-                <li>Price</li>
-                <li>Category</li>
-                <li>Action</li>
-            </ul>
-            {
-                products.map((item,index)=>
-                    <ul key={item._id}>
-                        <li>{index+1}</li> 
-                        <li>{item.name}</li> 
-                        <li>{item.price}</li>
-                        <li>{item.category}</li>
-                        <li><button onClick={()=>{navigate(`/update/${item._id}`)}}>Update</button><button onClick={()=>{deleteProduct(item._id)}}>Delete</button></li>
+            <input type="text" className="search-box" placeholder="Search Product" onChange={searchHandler}></input>
+            {products.length > 0 ?
+                <div>
+                    <ul>
+                        <li>S. No.</li> 
+                        <li>Name</li> 
+                        <li>Price</li>
+                        <li>Category</li>
+                        <li>Action</li>
                     </ul>
-                )
+                    {
+                        products.map((item,index)=>
+                            <ul key={item._id}>
+                                <li>{index+1}</li> 
+                                <li>{item.name}</li> 
+                                <li>{item.price}</li>
+                                <li>{item.category}</li>
+                                <li><button onClick={()=>{navigate(`/update/${item._id}`)}}>Update</button><button onClick={()=>{deleteProduct(item._id)}}>Delete</button></li>
+                            </ul>
+                        )
+                    }
+                </div> 
+                : <h1>No Products Found</h1>
             }
         </div>
     )
